8a079100ac6c21a560bbb3aae7fee7eb74f09c7f
zoltan.mohacsi <kismoha@hotmail.com>
2024-01-22 18:40:19 +0100

    Run source code formatter

--- src/test/java/bankmonitor/model/TransactionTest.java
+++ src/test/java/bankmonitor/model/TransactionTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 
 public class TransactionTest {
 
-  @Test
-  void test_getData() {
-    Transaction tr = new Transaction("{ \"reference\": \"foo\", \"amount\": 100}");
+    @Test
+    void test_getData() {
+        Transaction tr = new Transaction("{ \"reference\": \"foo\", \"amount\": 100}");
 
-    assertEquals(tr.getReference(), "foo");
-    assertEquals(tr.getAmount(), 300);
-  }
+        assertEquals(tr.getReference(), "foo");
+        assertEquals(tr.getAmount(), 300);
+    }
 }
