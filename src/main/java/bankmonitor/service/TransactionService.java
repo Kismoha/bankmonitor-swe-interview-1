ff643301ffe35d7a3ef9fed349879ca4ef7e4253
zoltan.mohacsi <kismoha@hotmail.com>
2024-01-23 17:30:25 +0100

    Create TransactionService interface

---
+++ src/main/java/bankmonitor/service/TransactionService.java
@@ -0,0 +1,15 @@
+package bankmonitor.service;
+
+import bankmonitor.model.Transaction;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface TransactionService {
+
+    List<Transaction> getAllTransactions();
+
+    Transaction createTransaction(String jsonData);
+
+    Transaction updateTransaction(Long id, String updateJSON);
+}
