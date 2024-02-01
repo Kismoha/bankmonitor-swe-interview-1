6c1c1936cd50719ca791fb1428e8d6175e04eae0
zoltan.mohacsi <kismoha@hotmail.com>
2024-02-01 18:42:09 +0100

    Add id getter and setter for hibernate

--- src/main/java/bankmonitor/model/Transaction.java
+++ src/main/java/bankmonitor/model/Transaction.java
@@ -38,6 +38,22 @@ public class Transaction {
         this.data = jsonObject.toString();
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
     public String getData() {
         return this.data;
     }
